bc05b79e184ddf3dcfbc7f5e32aa3d5c8a45160b
spursy <582214816@qq.com>
2017-08-17 20:48:49 +0800

    add function(receive message and reponse message)

--- server/wechat/reply.js
+++ server/wechat/reply.js
@@ -3,6 +3,29 @@ const tip = 'This is spursy, welcome to my fields\n' +
 
 export default async (ctx, next) => {
     const message = ctx.weixin
-    console.log(`${message}`)
+    // console.log(`${message}`)
     ctx.body = tip
+    if (message.MsgType == 'text') {
+        ctx.body = message.Content
+    } else if (message.MsgType == 'image') {
+        ctx.body = {
+            type: 'image',
+            mediaId: message.MediaId
+        }
+    } else if (message.MsgType == 'voice') {
+        ctx.body = {
+            type: 'image',
+            mediaId: message.MediaId
+        }
+    } else if (message.MsgType == 'video') {
+        ctx.body = {
+            title: message.ThumbMediaId,
+            type: 'video',
+            mediaId: message.MediaId
+        }
+    } else if (message.MsgType == 'location') {
+        ctx.body = message.Location_X + ' : ' + message.Location_Y + ' : ' + message.Label
+    } else if (message.MsgType == 'link') {
+        ctx.body = message.title
+    } 
 }
