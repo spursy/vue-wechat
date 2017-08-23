d4da083b27e12d2727366f7e0dfe39d94bdf2aca
spursy <582214816@qq.com>
2017-08-23 23:10:33 +0800

    add function(new sass file, update vue page as pug model)

--- store/actions.js
+++ store/actions.js
@@ -3,5 +3,23 @@ import Services from './services'
 export default {
     getWechatSignature({commit}, url) {
         return Services.getWechatSignature(url)
+    },
+
+    async fetchHouses ({state}) {
+        const res = await Services.fetchHouses()
+        state.houses = res.data.data
+        return res
+    },
+
+    async fetchCities ({state}) {
+        const res = await Services.fetchCities()
+        state.cities = res.data.data
+        return res
+    },
+
+    async fetchCharacters ({state}) {
+        const res = await Services.fetchHouses()
+        state.characters = res.data.data
+        return res 
     }
 }
