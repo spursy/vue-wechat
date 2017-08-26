c9d114b72401c309e6114cdd6abdd0da7dcbdfdc
spursy <582214816@qq.com>
2017-08-26 23:21:46 +0800

    remove test file.

--- server/api/wechat.js
+++ server/api/wechat.js
@@ -1,5 +1,5 @@
 import { getWeChat } from '../wechat'
-// add demo.
+
 export async function getSignatureAsync (url) {
     const client = await getWeChat()
     const data = await client.fetchAccessToken() 
